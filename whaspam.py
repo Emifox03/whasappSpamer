704c2fc19947f8eab5a1f78541d688b5cc269421
Emifox03 <79858068+Emifox03@users.noreply.github.com>
2022-10-20 19:33:23 +0200

    sjhcsbj

--- whaspam.py
+++ whaspam.py
@@ -10,12 +10,12 @@ webbrowser.open('https://api.whatsapp.com/send?phone='+num)
 
 pyautogui.sleep(3)
 
-for i in range(8):
+for i in range(7):
     pyautogui.press('tab')
 
 pyautogui.press('enter')
 
-for i in range(2):
+for i in range(1):
     pyautogui.press('tab')
     
 pyautogui.press('enter')
