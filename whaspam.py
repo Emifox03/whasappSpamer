3871c1497b6b1e1cff6c8ec56a2877db60f1a9f2
Emifox03 <79858068+Emifox03@users.noreply.github.com>
2022-10-20 19:47:32 +0200

    sfhjkcb

--- whaspam.py
+++ whaspam.py
@@ -8,10 +8,10 @@ num = input()
 #webbrowser.open('https://wa.me/15551234567')
 webbrowser.open('https://api.whatsapp.com/send?phone='+num)
 
-pyautogui.sleep(3)
-
+pyautogui.sleep(2)
 pyautogui.moveTo(673,326)
 pyautogui.click()
 
+pyautogui.sleep(2)
 pyautogui.moveTo(673,326)
 pyautogui.click()
