29dca058f8465ab05fa94a2ba2d2d4a28e96010d
Emifox03 <79858068+Emifox03@users.noreply.github.com>
2022-10-20 19:24:35 +0200

    sdihsiczx

--- whaspam.py
+++ whaspam.py
@@ -6,4 +6,4 @@ print("Introduce un numero de telefono")
 num = input()
 
 #webbrowser.open('https://wa.me/15551234567')
-webbrowser.open('https://wa.me/'+num)
+webbrowser.open(https://api.whatsapp.com/send?phone=num)
