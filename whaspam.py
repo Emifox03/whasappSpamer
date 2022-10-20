9f6ff05763319eb020722d547ce8811aac8894c5
Emifox03 <79858068+Emifox03@users.noreply.github.com>
2022-10-20 19:03:53 +0200

    kdjfvndk

--- whaspam.py
+++ whaspam.py
@@ -5,5 +5,5 @@ import webbrowser
 print("Introduce un numero de telefono")
 num = input()
 
-##webbrowser.open('https://wa.me/15551234567')
+#webbrowser.open('https://wa.me/15551234567')
 webbrowser.open('https://wa.me/'+num)
